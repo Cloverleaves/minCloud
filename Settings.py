390999714a89ae18fac0adc26be2c506e7ee1180
gimu <mail@gimu.org>
2015-03-28 11:45:56 +0100

    Add authentication Update frontend

--- Settings.py
+++ Settings.py
@@ -11,8 +11,17 @@ class Settings(object):
 
     # Default port is 9999
     PORT = configParser.getint('server', 'port') if configParser.get('server', 'port').isdigit() else 9999
+
+    # Paths
     # Default storing path is cloudstore, located in the same folder as this file
     CLOUD_PATH = configParser.get('path', 'store') if configParser.get('path', 'store') else os.path.join(dirname, 'cloudstore')
-
     STATIC_PATH = os.path.join(dirname, 'static')
     TEMPLATE_PATH = os.path.join(dirname, 'templates')
+
+    # You should generate your own key in config.ini
+    COOKIE_SECRET = configParser.get('auth', 'key') if configParser.get('auth', 'key') else "_GENERATE_YOUR_OWN_RANDOM_VALUE_"
+
+    # Authentication
+    # Default username and password: "admin"
+    USERNAME = configParser.get('auth', 'username') if configParser.get('auth', 'username') else "admin"
+    PASSWORD = configParser.get('auth', 'password') if configParser.get('auth', 'password') else "admin"
--- mincloud.py
+++ mincloud.py
@@ -16,6 +16,8 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
+            (r"/login", LoginHandler),
+            (r"/logout", LogoutHandler),
             (r"/mkdir", MkdirHandler),
             (r"/rename", RenameHandler),
             (r"/download", DownloadHandler),
@@ -26,13 +28,52 @@ class Application(tornado.web.Application):
         settings = {
             "template_path": Settings.TEMPLATE_PATH,
             "static_path": Settings.STATIC_PATH,
+            "cookie_secret": Settings.COOKIE_SECRET,
+            "login_url": "/login"
         }
         tornado.web.Application.__init__(self, handlers, **settings)
 
         print("Running minCloud on port %d" % Settings.PORT)
 
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
 
-class MainHandler(tornado.web.RequestHandler):
+class LoginHandler(BaseHandler):
+    def get(self):
+        try:
+            errormessage = self.get_argument("error")
+        except:
+            errormessage = ""
+        self.render("login.html", title="minCloud Login", errormessage=errormessage)
+
+    def check_permission(self, password, username):
+        return True if username == Settings.USERNAME and password == Settings.PASSWORD else False
+
+    def post(self):
+        username = self.get_argument('username', '')
+        password = self.get_argument('password', '')
+        auth = self.check_permission(password, username)
+        if auth:
+            self.set_current_user(username)
+            self.redirect("/")
+        else:
+            error_msg = u"?error=" + tornado.escape.url_escape("Incorrect login!")
+            self.redirect(u"/login" + error_msg)
+
+    def set_current_user(self, user):
+        if user:
+            self.set_secure_cookie("user", tornado.escape.json_encode(user))
+        else:
+            self.clear_cookie("user")
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect("/login")
+
+class MainHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         """Print dirs and files from current root."""
         listroot = os.path.join(Settings.CLOUD_PATH, self.get_argument('dir', ''))
@@ -55,16 +96,19 @@ class MainHandler(tornado.web.RequestHandler):
 
         self.render("index.html", title="minCloud", parentdir=parentdir, currentdir=self.get_argument('dir', ''), dirs=dirs, files=files)
 
-class ViewHandler(tornado.web.RequestHandler):
+class ViewHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         """Print out the given item."""
         target = self.get_argument('target', '')
+        
         content_type = mimetypes.guess_type(target)
         file = os.path.join(Settings.CLOUD_PATH, target)
         self.set_header('Content-Type', content_type[0] if content_type[0] is not None else 'text/plain') # Fix for invalid mime-types
         self.write(open(file, 'rb').read())
 
-class MkdirHandler(tornado.web.RequestHandler):
+class MkdirHandler(BaseHandler):
+    @tornado.web.authenticated
     def post(self):
         path = self.get_argument('path', '')
         directory = self.get_argument('directory', '') # New directory name
@@ -78,7 +122,8 @@ class MkdirHandler(tornado.web.RequestHandler):
 
         self.redirect("/?dir=" + tornado.escape.url_escape(path))
 
-class RenameHandler(tornado.web.RequestHandler):
+class RenameHandler(BaseHandler):
+    @tornado.web.authenticated
     def post(self):
         """Basic renaming for files and folders."""
         path = self.get_argument('path', '')
@@ -91,7 +136,8 @@ class RenameHandler(tornado.web.RequestHandler):
         else:
             os.rename(os.path.join(Settings.CLOUD_PATH, path, target), os.path.join(Settings.CLOUD_PATH, path, name))
 
-class UploadHandler(tornado.web.RequestHandler):
+class UploadHandler(BaseHandler):
+    @tornado.web.authenticated
     def post(self):
         """Upload file(s)."""
         path = self.get_argument('path', '')
@@ -108,20 +154,22 @@ class UploadHandler(tornado.web.RequestHandler):
         
         self.redirect("/?dir=" + tornado.escape.url_escape(path))
 
-class DownloadHandler(tornado.web.RequestHandler):
+class DownloadHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         """Download file by get method."""
         path = self.get_argument('path', '')
         target = self.get_argument('target', '')
 
         content_type = mimetypes.guess_type(target)
-        file = open(os.path.join(Settings.CLOUD_PATH, path, target) , 'rb')
+        file = os.path.join(Settings.CLOUD_PATH, path, target)
         self.set_header('Content-Type', content_type[0] if content_type[0] is not None else 'text/plain') # Fix for invalid mime-types
         self.set_header('Content-Disposition', 'attachment; filename=' + target + '')
-        self.write(file.read())
+        self.write(open(file, 'rb').read())
         
 
-class DeleteHandler(tornado.web.RequestHandler):
+class DeleteHandler(BaseHandler):
+    @tornado.web.authenticated
     def post(self):
         path = self.get_argument('path', '')
         target = self.get_argument('target', '')
